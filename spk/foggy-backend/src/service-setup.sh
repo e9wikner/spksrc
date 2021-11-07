eec596f4356bff5e0b5f9a8a459d85788bb36797
Stefan Wikner <stefan@stefanwikner.se>
2021-11-07 19:33:59 +0100

    Initial foggy-backend commit

---
+++ spk/foggy-backend/src/service-setup.sh
@@ -0,0 +1,21 @@
+
+PYTHON_DIR="/var/packages/python3/target/bin"
+PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}:${PATH}"
+VIRTUALENV="${PYTHON_DIR}/python3 -m venv"
+PIP=${SYNOPKG_PKGDEST}/env/bin/pip3
+
+service_postinst ()
+{
+    # Create a Python virtualenv
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env
+
+    # Install the wheels
+    wheelhouse=${SYNOPKG_PKGDEST}/share/wheelhouse
+    ${PIP} install --no-deps --no-index --upgrade --force-reinstall --find-links ${wheelhouse} ${wheelhouse}/*.whl
+
+    # Log installation information
+    echo "Installed version: $(${SYNOPKG_PKGDEST}/env/bin/foggy version 2>&1)"
+    echo "Installed python modules:"
+    ${PIP} freeze
+}
+
